0c60608c2cb3da7c48e1441497efab06757a7dca
Melody Soriano <mel.soriano6123@gmail.com>
2020-06-04 02:25:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -71,8 +71,8 @@ class Person
 
   def start_conversation(person, topic)
     if topic == "politics"
-      self.happiness = @happiness - 1
-      person.happiness = person.happiness - 1
+      self.happiness = @happiness - 2
+      person.happiness = person.happiness - 2
       "blah blah partisan blah lobbyist"
     elsif topic == "weather"
       self.happiness = @happiness + 1
